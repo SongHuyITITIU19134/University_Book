118462d974720a9231f505925eb32c2ed554a694
SongHuyITITIU19134 <80945626+SongHuyITITIU19134@users.noreply.github.com>
2025-03-12 22:09:28 +0700

    Coding the Layout and Auth UI with Using Zod + React-Hook-Form

--- constans/index.ts
+++ constans/index.ts
@@ -40,20 +40,20 @@
 //     },
 //   ];
   
-//   export const FIELD_NAMES = {
-//     fullName: "Full name",
-//     email: "Email",
-//     universityId: "University ID Number",
-//     password: "Password",
-//     universityCard: "Upload University ID Card",
-//   };
+  export const FIELD_NAMES = {
+    fullName: "Full name",
+    email: "Email",
+    universityId: "University ID Number",
+    password: "Password",
+    universityCard: "Upload University ID Card",
+  };
   
-//   export const FIELD_TYPES = {
-//     fullName: "text",
-//     email: "email",
-//     universityId: "number",
-//     password: "password",
-//   };
+  export const FIELD_TYPES = {
+    fullName: "text",
+    email: "email",
+    universityId: "number",
+    password: "password",
+  };
   
   export const sampleBooks = [
     {
