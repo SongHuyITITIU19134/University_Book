03a7b0cb23bac67876f379093f82711924dc7499
SongHuyITITIU19134 <80945626+SongHuyITITIU19134@users.noreply.github.com>
2025-03-23 16:18:37 +0700

    update the code in config and package

--- next.config.ts
+++ next.config.ts
@@ -1,30 +1,29 @@
-import type { NextConfig } from "next";
+import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
-    remotePatterns:
-      [
-        {
-          protocol: "https",
-          hostname: "placehold.co"
-        },
-        {
-          protocol: 'https',
-          hostname: "m.media-amazon.com"
-        },
-        {
-          protocol: 'https',
-          hostname: "ik.imagekit.io",
-          port: "",
-        }
-      ]
-  }, typescript: {
-    ignoreBuildErrors: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
+      {
+        protocol: "https",
+        hostname: "m.media-amazon.com",
+      },
+      {
+        protocol: "https",
+        hostname: "ik.imagekit.io",
+      },
+    ],
+  },
+  typescript: {
+    ignoreBuildErrors: false,
   },
   eslint: {
-    ignoreDuringBuilds: true,
-  }
+    ignoreDuringBuilds: false,
+  },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
